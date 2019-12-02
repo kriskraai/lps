2313f4ec5b3e21f0cf6c9496c85cdba67854eaf6
kriskraai <mychristo@gmail.com>
2019-12-02 15:17:35 +0200

    first push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./components/app";
+import PhotoGrid from "./components/photogrid";
+import Single from "./components/single";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { Provider } from "react-redux";
+import { PropsRoute } from "./components/router";
+import store from "./store";
+
+const router = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <PropsRoute exact path="/" component={PhotoGrid} />
+        <PropsRoute path="/view/:postId" component={Single} />
+      </App>
+    </BrowserRouter>
+  </Provider>
+);
+
+render(router, document.getElementById("app"));
